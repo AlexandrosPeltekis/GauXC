fbb6ce599d015117fcffe6100abe822fe59972ca
aodongliu <al777@uw.edu>
2023-10-24 12:45:18 -0700

    fix factor of 2 in electronic EPC

--- src/xc_integrator/replicated/host/reference_replicated_xc_host_integrator_exc_vxc.hpp
+++ src/xc_integrator/replicated/host/reference_replicated_xc_host_integrator_exc_vxc.hpp
@@ -992,7 +992,7 @@ void ReferenceReplicatedXCHostIntegrator<ValueType>::
 
       // Update electronic eps and vxc
       eps[iPt]      += -1.0 * total_prho/dn;
-      vrho[iPt]     +=  2 * ( -1.0 * total_prho / dn + (-1.2 * std::sqrt(total_erho) * std::sqrt(total_prho) * total_prho 
+      vrho[iPt]     +=  ( -1.0 * total_prho / dn + (-1.2 * std::sqrt(total_erho) * std::sqrt(total_prho) * total_prho 
                         + 6.6 * total_erho * total_prho * total_prho ) / (dn * dn) );
 
       // Assign protonic eps and vxc
@@ -1066,7 +1066,7 @@ void ReferenceReplicatedXCHostIntegrator<ValueType>::
   
   }  // End OpenMP region
 
-  std::cout << "N_EL = " << std::setprecision(12) << std::scientific << *N_EL << std::endl;
+  //std::cout << "N_EL = " << std::setprecision(12) << std::scientific << *N_EL << std::endl;
   //std::cout << "EXC1 = " << std::setprecision(12) << std::scientific << *EXC1 << std::endl
   //std::cout << "EXC2 = " << std::setprecision(12) << std::scientific << *EXC2 << std::endl;
 
