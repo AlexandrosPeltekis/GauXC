fbf7bd35a9b490e702ec495e223b961f455f0358
Aodong Liu <al777@uw.edu>
2023-10-30 22:48:43 -0700

    pass another basis to the load balancer

--- include/gauxc/xc_integrator/replicated/replicated_xc_integrator_impl.hpp
+++ include/gauxc/xc_integrator/replicated/replicated_xc_integrator_impl.hpp
@@ -98,24 +98,24 @@ public:
                      value_type* EXC );
   
   void neo_eval_exc_vxc( int64_t m1, int64_t n1, int64_t m2, int64_t n2, 
-                     const value_type* P1s, int64_t ldp1s,
-                     const value_type* P2s, int64_t ldp2s,
-                     const value_type* P2z, int64_t ldp2z,
-                     value_type* VXC1s, int64_t ldvxc1s,
-                     value_type* VXC2s, int64_t ldvxc2s,
-                     value_type* VXC2z, int64_t ldvxc2z,
-                     value_type* EXC );
+                         const value_type* P1s, int64_t ldp1s,
+                         const value_type* P2s, int64_t ldp2s,
+                         const value_type* P2z, int64_t ldp2z,
+                         value_type* VXC1s, int64_t ldvxc1s,
+                         value_type* VXC2s, int64_t ldvxc2s,
+                         value_type* VXC2z, int64_t ldvxc2z,
+                         value_type* EXC );
 
   void neo_eval_exc_vxc( int64_t m1, int64_t n1, int64_t m2, int64_t n2, 
-                     const value_type* P1s, int64_t ldp1s,
-                     const value_type* P1z, int64_t ldp1z,
-                     const value_type* P2s, int64_t ldp2s,
-                     const value_type* P2z, int64_t ldp2z,
-                     value_type* VXC1s, int64_t ldvxc1s,
-                     value_type* VXC1z, int64_t ldvxc1z,
-                     value_type* VXC2s, int64_t ldvxc2s,
-                     value_type* VXC2z, int64_t ldvxc2z,
-                     value_type* EXC );
+                         const value_type* P1s, int64_t ldp1s,
+                         const value_type* P1z, int64_t ldp1z,
+                         const value_type* P2s, int64_t ldp2s,
+                         const value_type* P2z, int64_t ldp2z,
+                         value_type* VXC1s, int64_t ldvxc1s,
+                         value_type* VXC1z, int64_t ldvxc1z,
+                         value_type* VXC2s, int64_t ldvxc2s,
+                         value_type* VXC2z, int64_t ldvxc2z,
+                         value_type* EXC );
 
   void eval_exc_grad( int64_t m, int64_t n, const value_type* P,
                       int64_t ldp, value_type* EXC_GRAD );
